df33541414b77c5ddfec41d2f76b7ffb9f35caa6
Atsushi Osawa <a24osawa@gmail.com>
2022-06-17 21:52:11 +0900

    Initial Commit

---
+++ BlogTest/BlogTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlogTestApp.swift
+//  BlogTest
+//
+//  Created by Atsushi Osawa on 2022/06/17.
+//
+
+import SwiftUI
+
+@main
+struct BlogTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
